50a387f14df54416d6ffaa2a82c13cf89477a7e7
PatricioDU <pata.gonzalez@duocuc.cl>
2024-10-21 10:03:50 -0300

    foro component e imagenes

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -3,34 +3,35 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Asistencia } from 'src/app/interfaces/asistencia';
 import { Usuario } from 'src/app/model/usuario';
 
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
-export class InicioPage implements OnInit {
+export class InicioPage {
 
-@ViewChild('video') private video!: ElementRef;
+  @ViewChild('video') private video!: ElementRef;
   @ViewChild('canvas') private canvas!: ElementRef;
   @ViewChild('titulo', { read: ElementRef }) itemTitulo!: ElementRef;
 
-  public usuario: Usuario;
-  public asistencia: Asistencia = new Asistencia();
-  public escaneando = false;
-  public datosQR: string = '';
-  animationController: any;
+  // public usuario: Usuario;
+  // public asistencia: Asistencia = new Asistencia();
+  // public escaneando = false;
+  // public datosQR: string = '';
+  // animationController: any;
   
 
-  constructor(private activatedRoute: ActivatedRoute, private router: Router) { 
-    this.usuario = new Usuario();
-    this.usuario.recibirUsuario(activatedRoute, router);
-  }
+  // constructor(private activatedRoute: ActivatedRoute, private router: Router) { 
+  //   this.usuario = new Usuario();
+  //   this.usuario.recibirUsuario(activatedRoute, router);
+  // }
 
-  ngOnInit() {
-  }
-  navegar(pagina: string) {
-    this.usuario.navegarEnviandousuario(this.router, pagina);
-  }
+  // ngOnInit() {
+  // }
+  // navegar(pagina: string) {
+  //   this.usuario.navegarEnviandousuario(this.router, pagina);
+  // }
 
 
 
