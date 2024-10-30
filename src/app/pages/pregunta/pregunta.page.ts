5accd0b61b5b891c3690d46a226298ab5f3a8ce0
PatricioDU <pata.gonzalez@duocuc.cl>
2024-10-30 09:49:01 -0300

    ahora si

--- src/app/pages/pregunta/pregunta.page.ts
+++ src/app/pages/pregunta/pregunta.page.ts
@@ -2,50 +2,27 @@ import { ToastController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Usuario } from 'src/app/model/usuario';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-pregunta',
   templateUrl: './pregunta.page.html',
   styleUrls: ['./pregunta.page.scss'],
-})
-export class PreguntaPage implements OnInit {
-  
-  public usuario: Usuario;
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonicModule,
+    FormsModule,
+]})
+export class PreguntaPage {
 
-  constructor(
-    private activatedRoute: ActivatedRoute,
-    private ToastController: ToastController,
-    private router: Router) 
+
+  constructor() 
   { 
 
-    this.usuario = new Usuario();
-    this.usuario.recibirUsuario(this.activatedRoute, this.router);
   }
 
-  ngOnInit() {
-  }
-  ingresar() {
-    debugger
-    // Llama a la función que valida la respuesta secreta
-    const error = this.usuario.validarRespuestaSecreta();
-    
-    // Si hay un error, muestra el mensaje emergente y redirige al usuario a la página de "incorrecto"
-    if(error) {
-      this.mostrarMensajeEmergente(error);
-      this.usuario.navegarEnviandousuario(this.router, '/incorrecto');
-      return;
-    }
-    
-    // Si la respuesta es correcta, muestra el mensaje con la contraseña y redirige a "correcto"
-    this.mostrarMensajeEmergente(this.usuario.password);
-    this.usuario.navegarEnviandousuario(this.router, '/correcto');
-  }
-  
-  async mostrarMensajeEmergente(mensaje: string, duracion?: number) {
-    const toast = await this.ToastController.create({
-        message: mensaje,
-        duration: duracion? duracion: 2000
-      });
-    toast.present();
-  }
+
 }
