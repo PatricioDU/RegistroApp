906f4ed1e058056b2fb93a3301c2a414dba7135e
TuTooH <135929858+TuTooH@users.noreply.github.com>
2024-11-03 08:36:09 -0300

    .

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { IonicModule } from '@ionic/angular';
@@ -7,6 +7,7 @@ import { NavController } from '@ionic/angular';
 import { arrowBackOutline, logOut, logOutOutline, qrCodeOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 import { AuthService } from 'src/app/services/auth.service';
+import { AnimationController} from '@ionic/angular';
 
 @Component({
   selector: 'app-header',
@@ -21,15 +22,27 @@ import { AuthService } from 'src/app/services/auth.service';
   ]
 })
 export class HeaderComponent {
+  @ViewChild('titulo', { read: ElementRef }) itemTitulo!: ElementRef;
 
   @Output() headerClick = new EventEmitter<string>();
 
-  constructor(private navCtrl: NavController, private authService: AuthService) { 
+  constructor(private navCtrl: NavController, private authService: AuthService,private animationController: AnimationController) { 
     addIcons({ logOutOutline, qrCodeOutline });
   }
 
   sendClickEvent(buttonName: string) {
     this.headerClick.emit(buttonName);
   }
+
+  animarTituloIzqDer() {
+    this.animationController
+    .create()
+    .addElement(this.itemTitulo.nativeElement)
+    .iterations(Infinity) // Repite la animación infinitamente
+    .duration(5000) // Duración de la animación en milisegundos (2 segundos)
+    .fromTo('opacity', 0, 1) // Cambia la opacidad de 0 (invisible) a 1 (visible)
+    .play();
+  }
+
 
 }
