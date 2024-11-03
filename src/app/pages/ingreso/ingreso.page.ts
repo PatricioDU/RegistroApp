74d66a73778f4d5e9e4920cd982fcdf509d069e3
TuTooH <135929858+TuTooH@users.noreply.github.com>
2024-11-03 19:39:44 -0300

    traducciones

--- src/app/pages/ingreso/ingreso.page.ts
+++ src/app/pages/ingreso/ingreso.page.ts
@@ -2,13 +2,13 @@ import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-// import { ViewWillEnter } from '@ionic/angular';
+import { ViewWillEnter } from '@ionic/angular';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { LanguageComponent } from 'src/app/components/language/language.component';
 import { Router } from '@angular/router';
-// import { colorWandOutline } from 'ionicons/icons';
-// import { addIcons } from 'ionicons';
-import { AuthService } from 'src/app/services/auth.service'
+import { colorWandOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-ingreso',
   templateUrl: './ingreso.page.html',
@@ -22,14 +22,20 @@ import { AuthService } from 'src/app/services/auth.service'
     , LanguageComponent       // CGV-Lista de idiomas
   ]
 })
+
+
+
+
 export class ingresoPage {
 
+  @ViewChild('selectLanguage') selectLanguage!: LanguageComponent;
   password: string;
   correo: string;
 
   constructor( 
     private authService: AuthService
-   , private router: Router) 
+   , private router: Router,
+   private translate: TranslateService) 
   {
     this.correo = 'atorres';
     this.password = '1234';
@@ -45,6 +51,10 @@ export class ingresoPage {
 
   navigateTheme() {
     this.router.navigate(['/theme']);
+  }
+
+  async ionViewWillEnter() {
+    this.selectLanguage.setCurrentLanguage();
   }
 
 }
