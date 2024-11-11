3181fb3f7d90eef6c375e0abd6aa8f2f1205bbe7
TuTooH <135929858+TuTooH@users.noreply.github.com>
2024-11-11 10:53:28 -0300

    .

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 rem ionic cap run android -l --external --host=192.168.54.179 --port=8100 --target AF2S024418000438
-ionic cap run android -l --external --host=192.168.54.27 --port=8100 --target=AF2S024418000438
+ionic cap run android -l --external --host=192.168.54.179 --port=8100 --target=AF2S024418000438
