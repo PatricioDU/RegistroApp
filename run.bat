ecf97077cb1895f2b0f2e1dd0ef02aca9ca14bb4
PatricioDU <pata.gonzalez@duocuc.cl>
2024-11-25 10:01:25 -0300

    otro

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 rem ionic cap run android -l --external --host=192.168.64.158 --port=8100 --target lr65g6dyyluc7dtg
-ionic cap run android -l --external --host=192.168.64.158 --port=8100 --target=lr65g6dyyluc7dtg
+ionic cap run android -l --external --host=192.168.216.125 --port=8100 --target=lr65g6dyyluc7dtg
--- src/app/services/apiclient.service.ts
+++ src/app/services/apiclient.service.ts
@@ -17,8 +17,8 @@ export class APIClientService {
     })
   };
 
-   apiUrl = 'http://localhost:3000';
-  //  apiUrl = 'http://192.168.64.158';
+  // apiUrl = 'http://localhost:3000';
+    apiUrl = 'http://192.168.216.125';
   postList: BehaviorSubject<Post[]> = new BehaviorSubject<Post[]>([]);
 
   constructor(private http: HttpClient) { }
